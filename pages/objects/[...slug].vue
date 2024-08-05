0525cef076a9e50a35f297c334ed18f97198a21f
Brandon K. Waltz <bwaltz6@gmail.com>
2024-08-05 16:35:34 -0400

    debug

--- pages/objects/[...slug].vue
+++ pages/objects/[...slug].vue
@@ -44,17 +44,15 @@ export default {
     const route = useRoute();
     const todo = ref({})
 
-    // useFetch(() => 
-    //   axios.get(`https://ccma-search-proof-8365887253.us-east-1.bonsaisearch.net/stage/_source/https:%2F%2Fmuseum-data.colby.edu%2Fcollection%2Fembark%2FObjects-1%2Finfo%2F${encodeURIComponent(route.params.slug[0])}`, {
-    //     auth: {
-    //       username: 'Fr2fpegcBZ',
-    //       password: 'Vi7vGnL3h2rtW5SuECoKRwTf'
-    //     }
-    //   })
-    //   .then((output) => (todo.value = output.data))
-    // )
-
-    todo.value = [];
+    useFetch(() => 
+      axios.get(`https://ccma-search-proof-8365887253.us-east-1.bonsaisearch.net/stage/_source/https:%2F%2Fmuseum-data.colby.edu%2Fcollection%2Fembark%2FObjects-1%2Finfo%2F${encodeURIComponent(route.params.slug[0])}`, {
+        auth: {
+          username: 'Fr2fpegcBZ',
+          password: 'Vi7vGnL3h2rtW5SuECoKRwTf'
+        }
+      })
+      .then((output) => (todo.value = output.data))
+    )
 
     // console.log(todo.value);
 
