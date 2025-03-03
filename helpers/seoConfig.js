1d765367fc44b72955c0075edd427c2bb0d22815
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2025-03-03 12:53:10 -0500

    OG Image testing

--- helpers/seoConfig.js
+++ helpers/seoConfig.js
@@ -1,6 +1,10 @@
+import axios from "axios";
+import { useInterfaceStore } from "~/store/interface";
+
 const pageSEO = (props, type) => {
   const route = useRoute();
   const todo = ref({});
+  let imagePath;
 
   // console.log(route);
 
@@ -8,16 +12,33 @@ const pageSEO = (props, type) => {
     fetch(`${props.interface.endpoint}${type ? type : 'pages'}?slug=${route.params.slug ? route.params.slug : 'home'}`)
       .then(res => res.json())
       .then((output) => (todo.value = output[0]))
-  )
+      .then(async () => { 
+        let imageObj;
   
-  // console.log(todo.value);
+        await axios
+          .get(`${useInterfaceStore().endpoint}media/${todo.value.featured_media}`)
+          .then((output) => {
+            imageObj = output.data;
+            imagePath = imageObj ? imageObj.media_details.sizes.desktop.source_url : '';
+            console.log(imagePath);
+          })
+          .then(() => {
+            useSeoMeta({
+              ogTitle: () => `${todo.value.title ? todo.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art · Colby College`,
+              title: () => `${todo.value.title ? todo.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art · Colby College`,
+              ogDescription: () => todo.value.excerpt?.rendered.replace(/<p[^>]*>|<\/p>/g, ''),
+              description: () => todo.value.excerpt?.rendered.replace(/<p[^>]*>|<\/p>/g, ''),
+              ogImage: () => `${imagePath}`,
+            });
+          });
+       })
+      .then(() => {
+        // console.log('HERE');
+        // console.log(todo.value);
 
-  useSeoMeta({
-    ogTitle: () => `${todo.value.title ? todo.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art · Colby College`,
-    title: () => `${todo.value.title ? todo.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art · Colby College`,
-    ogDescription: () => todo.value.excerpt?.rendered,
-    description: () => todo.value.excerpt?.rendered,
-  });
+        
+      })
+  )
 }
 
 export default pageSEO;
