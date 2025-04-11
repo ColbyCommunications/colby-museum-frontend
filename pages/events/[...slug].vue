83297fb127b68690b17a78275bf56d4750717887
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2025-04-10 23:16:39 -0400

    am pm adjustment

--- pages/events/[...slug].vue
+++ pages/events/[...slug].vue
@@ -146,7 +146,7 @@ export default {
       const sec = parseInt(time[2]);
       const ampm = (hour >= 12) ? " p.m." : " a.m.";
 
-      return `${hour == 12 || hour == 0 ? 12 : hour % 12}:${min.replace(/\s/g, '').replace('am', '').replace('pm', '')}${ampm}`;
+      return `${hour == 12 || hour == 0 ? 12 : hour % 12}:${min.replace(/\s/g, '').replace('am', ' a.m.').replace('pm', ' p.m.')}`;
     }
   }
 }
