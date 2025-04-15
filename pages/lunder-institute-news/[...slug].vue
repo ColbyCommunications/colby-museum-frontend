2febdd10fd292fc863cf2c90bc3f18b071994f25
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2025-04-15 14:58:05 -0400

    lunder institute news

---
+++ pages/lunder-institute-news/[...slug].vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="page page--default">
+    <Breadcrumbs
+      v-if="$route.params.slug != ''"
+      :items="breadcrumbs"
+      :current="{
+        title: title,
+        url: fullPath,
+      }"
+      :manual="{
+        title: 'News',
+        url: '/news/page-1',
+      }"
+    />
+    <IntroContext
+      :heading="title"
+      :headingElement="'h1'"
+      :subheading="excerpt"
+    />
+    <BaseModule
+      v-for="(item, index) in components"
+      :moduleData="item"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import transitionConfig from '../helpers/transitionConfig';
+import seoConfig from '../helpers/seoConfig';
+
+export default {
+  setup(props) {
+    seoConfig(props, 'posts');
+
+    definePageMeta({
+      pageTransition: transitionConfig,
+    });
+  },
+  data() {
+    return {
+      title: undefined,
+      excerpt: undefined,
+      components: undefined,
+    };
+  },
+  props: {
+    interface: {
+      required: false,
+    },
+  },
+  async mounted() {
+    const page = this;
+
+    // console.log(this.$route.params.slug);
+
+    await axios
+      .get(`${this.interface.endpoint}posts?slug=${this.$route.params.slug}`)
+      .then((output) => {
+        const post = output.data[0];
+
+        page.title = post.title.rendered
+          .replace(/–/g, '-')
+          .replace(/“/g, '"')
+          .replace(/”/g, '"')
+          .replace(/’/g, "'");
+        
+        page.excerpt = post.excerpt.rendered.replace(/<\/?[^>]+(>|$)/g, '');
+
+        page.components = post.block_data.map((component) => {
+          
+          component.type = component.blockName
+            .replace('acf/','')
+            .replace(/\//g,'-');
+
+          return {
+            type: component.type,
+            ...component.attrs.data,
+            attrs: component.attrs.data ? undefined : component.attrs,
+          };
+        });
+      });
+  }
+}
+</script>
