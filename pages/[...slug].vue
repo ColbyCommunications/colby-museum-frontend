0cf6a5475a6c6cae56ae16cb6a71255cb94896b3
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2025-04-22 15:13:38 -0400

    Speeding up refresh

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -115,14 +115,13 @@ export default {
       if (timeLeft.value === 1) {
         if (this.hasFileExtension(this.$route.path)) {
           navigateTo(`${this.interface.backend}${this.$route.path}`, {
-            external: true,
-            target: '_blank',
+            external: true
           })
         }
       }
 
       timeLeft.value = timeLeft.value - 1;
-    }, 500);
+    }, 100);
   },
   methods: {
     async getBreadcrumbs(post) {
