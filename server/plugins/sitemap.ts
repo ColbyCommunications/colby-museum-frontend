ec9fc9a93b0757c7e018c98f945802bb1d72fe25
Brandon K. Waltz <bwaltz6@gmail.com>
2025-05-02 07:45:22 -0400

    Update sitemap.ts

--- server/plugins/sitemap.ts
+++ server/plugins/sitemap.ts
@@ -4,7 +4,7 @@ export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('sitemap:output', async (ctx) => {
         ctx.sitemap = ctx.sitemap.replaceAll(
             'https://museum-backend.colby.edu/',
-            'https://colby-museum-frontend.pages.dev/'
+            'https://colby-cf.colby.edu/'
         );
     });
 });
