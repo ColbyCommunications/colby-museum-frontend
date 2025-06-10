ba4ac6503a68a82aadcdc53d07c37918c6e959c8
Charlie Butcosk <incharge@weichbild.com>
2025-06-10 16:26:08 -0400

    Correct composable handling for page meta

--- helpers/seoConfig.js
+++ helpers/seoConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useInterfaceStore } from "~/store/interface";
 
 const pageSEO = async (props, type) => {
+  const nuxtApp = useNuxtApp()
   const route = useRoute();
   const pageMeta = ref();
 
@@ -9,7 +10,7 @@ const pageSEO = async (props, type) => {
   const endpointType = type ? type : 'pages'
   const endpointUrl = computed( () => `${props.interface.endpoint}${endpointType}` )
 
-  const { data, error, status } = await useFetch(endpointUrl.value, { immediate: false, lazy: true, method: 'GET', query: { slug, _embed: 'wp:featuredmedia' } })
+  const { data, error, status } = await useFetch(endpointUrl.value, { method: 'GET', query: { slug, _embed: 'wp:featuredmedia' } })
 
   if (error.value) {
     console.error(`Encountered an error when fetching page metadata from ${endpointUrl.value}:`,error)
@@ -26,13 +27,17 @@ const pageSEO = async (props, type) => {
   const { _embedded } = pageMeta.value
   let postImageUrl = `${useInterfaceStore().backend}wp-content/uploads/2025/03/default.jpg`
 
-  useSeoMeta({
+  // Because the component may have lost the app context, request it before setting meta.
+  // Without this line the composable will error out, preventing prerendering meta tags 
+
+  // See https://nuxt.com/docs/api/composables/use-nuxt-app#runwithcontext for more
+  nuxtApp.runWithContext(() => useSeoMeta({
     ogTitle: () => `${pageMeta.value.title ? pageMeta.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&amp;', '&').replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art`,
     title: () => `${pageMeta.value.title ? pageMeta.value.title?.rendered.replace(/&quot;/g, '\"').replace('&#8217;',"'").replace('&amp;', '&').replace('&#038;',"&") + ' | ' : ''}Colby College Museum of Art`,
     ogDescription: () => pageMeta.value.excerpt?.rendered.replace(/<p[^>]*>|<\/p>/g, ''),
     description: () => pageMeta.value.excerpt?.rendered.replace(/<p[^>]*>|<\/p>/g, ''),
     ogImage: () => postImageUrl,
-  });
+  }));
 }
 
 export default pageSEO;
