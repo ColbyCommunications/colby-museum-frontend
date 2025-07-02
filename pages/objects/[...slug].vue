ab4063ece8bd6201c5bcee92d58352c911a6ad73
Charlie Butcosk <incharge@weichbild.com>
2025-07-02 14:53:12 -0400

    Convert objects slug handler to use composition + composables

--- pages/objects/[...slug].vue
+++ pages/objects/[...slug].vue
@@ -61,11 +61,6 @@ const setPageMeta = async () => {
       return
     }
 
-    // if (!data.value) {
-    //   console.error(`Received empty head meta from the endpoint!`)
-    //   return
-    // }
-
     pageMeta.value = data.value
     const fallbackImage = computed( () => `${useInterfaceStore().backend}wp-content/uploads/2025/03/default.jpg`)
 
@@ -78,104 +73,86 @@ const setPageMeta = async () => {
         ogImage: () => pageMeta.value?.Images?.length > 0 ? `https://ccma-iiif-cache-service.fly.dev/iiif/2/${pageMeta.value.Images[0].IIIF_URL.substring(pageMeta.value.Images[0].IIIF_URL.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "")}/full/${encodeURIComponent('400,')}/0/default.jpg` : fallbackImage ,
       });
     }) 
-    
+
+    return data    
 }
 
 export default {
-  setup(props) {
-    setPageMeta()
-
+  async setup(props) {
     definePageMeta({
       pageTransition: transitionConfig,
     });
-  },
-  data() {
+
+    const data = await setPageMeta()
+
+    const post = data.value;
+
+    let title = post.Disp_Title === '' ? 'Untitled' : post.Disp_Title
+                                                        .replace(/–/g, '-')
+                                                        .replace(/“/g, '"')
+                                                        .replace(/”/g, '"')
+                                                        .replace(/’/g, "'")
+
+    const artist = post.Disp_Maker_1;
+    const period = post.Disp_Create_DT;
+    
+    let images = []    
+    if (post.Images.length > 0) {
+      images = post.Images.map((i) => ({
+        image: {
+          caption: {
+            rendered: `<strong>${post.Disp_Maker_1}</strong>, <i>${post.Disp_Title}</i>, ${post.Disp_Create_DT}. ${post.Disp_Medium}${post.Disp_Dimen ? ', ' + post.Disp_Dimen : ''}. Colby College Museum of Art${post.Dedication ? ', ' + post.Dedication : ''}; ${post.Disp_Access_No}`,
+          },
+          alt_text: post.Disp_Medium,
+          media_details: {
+            sizes: {
+              full: {
+                source_url: `https://ccma-iiif-cache-service.fly.dev/iiif/2/${i.IIIF_URL.substring(i.IIIF_URL.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "")}/full/1800%2C/0/default.jpg`,
+              },
+              mobile: {
+                source_url: `https://ccma-iiif-cache-service.fly.dev/iiif/2/${i.IIIF_URL.substring(i.IIIF_URL.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "")}/full/400%2C/0/default.jpg`,
+              }
+            }
+          }
+        }
+      }))
+    }
+
+    const items = [
+      {
+        heading: 'Object Type',
+        paragraph: post.Disp_Obj_Type,
+      },
+      {
+        heading: 'Medium',
+        paragraph: post.Disp_Medium,
+      },
+      {
+        heading: 'Dimensions',
+        paragraph: post.Disp_Dimen,
+      },
+      {
+        heading: 'Credit Line',
+        paragraph: post.Dedication,
+      },
+      {
+        heading: 'Accession Number',
+        paragraph: post.Disp_Access_No,
+      },
+    ]
+
     return {
-      title: undefined,
-      artist: undefined,
-      period: undefined,
-      images: undefined,
-      items: undefined,
-    };
+      title,
+      artist,
+      period,
+      images,
+      items,
+    };    
   },
   props: {
     interface: {
       required: false,
     },
   },
-  async mounted() {
-    const page = this;
-
-    // console.log(encodeURIComponent(this.$route.params.slug[0]));
-    console.log(`https://ccma-search-proof-8365887253.us-east-1.bonsaisearch.net/stage/_source/object%2F${encodeURIComponent(this.$route.params.slug[0])}`);
-
-    await axios
-      .get(`https://ccma-search-proof-8365887253.us-east-1.bonsaisearch.net/stage/_source/object%2F${encodeURIComponent(this.$route.params.slug[0])}`, {
-        auth: {
-          username: 'Fr2fpegcBZ',
-          password: 'Vi7vGnL3h2rtW5SuECoKRwTf'
-        }
-      })
-      .then((output) => {
-        console.log(output.data);
-        const post = output.data;
-
-        page.title = post.Disp_Title
-          .replace(/–/g, '-')
-          .replace(/“/g, '"')
-          .replace(/”/g, '"')
-          .replace(/’/g, "'");
-
-        page.title == '' ? page.title = 'Untitled' : page.title;
-        page.artist = post.Disp_Maker_1;
-        page.period = post.Disp_Create_DT;
-
-        // page.images = post.Images.length > 0 ? `https://ccma-iiif-cache-service.fly.dev/iiif/2/${post.Disp_Access_No}_001_cd/full/2200%2C/0/default.jpg` : undefined;
-        
-        if (post.Images.length > 0) {
-          page.images = post.Images.map((i) => ({
-            image: {
-              caption: {
-                rendered: `<strong>${post.Disp_Maker_1}</strong>, <i>${post.Disp_Title}</i>, ${post.Disp_Create_DT}. ${post.Disp_Medium}${post.Disp_Dimen ? ', ' + post.Disp_Dimen : ''}. Colby College Museum of Art${post.Dedication ? ', ' + post.Dedication : ''}; ${post.Disp_Access_No}`,
-              },
-              alt_text: post.Disp_Medium,
-              media_details: {
-                sizes: {
-                  full: {
-                    source_url: `https://ccma-iiif-cache-service.fly.dev/iiif/2/${i.IIIF_URL.substring(i.IIIF_URL.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "")}/full/1800%2C/0/default.jpg`,
-                  },
-                  mobile: {
-                    source_url: `https://ccma-iiif-cache-service.fly.dev/iiif/2/${i.IIIF_URL.substring(i.IIIF_URL.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, "")}/full/400%2C/0/default.jpg`,
-                  }
-                }
-              }
-            }
-          }))
-        }
-
-        page.items = [
-          {
-            heading: 'Object Type',
-            paragraph: post.Disp_Obj_Type,
-          },
-          {
-            heading: 'Medium',
-            paragraph: post.Disp_Medium,
-          },
-          {
-            heading: 'Dimensions',
-            paragraph: post.Disp_Dimen,
-          },
-          {
-            heading: 'Credit Line',
-            paragraph: post.Dedication,
-          },
-          {
-            heading: 'Accession Number',
-            paragraph: post.Disp_Access_No,
-          },
-        ]
-      });
-  }
 }
 </script>
