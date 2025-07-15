b97ed710cb9603294e1f44e2793d24eb52351030
Charlie Butcosk <incharge@weichbild.com>
2025-07-15 16:53:02 -0400

    Remove animation as test

--- components/Context.vue
+++ components/Context.vue
@@ -229,7 +229,7 @@ export default {
   //   this.interface = useInterfaceStore();
   // },
   mounted() {
-    this.animate();
+    // this.animate();
 
     let previousHeight = document.body.scrollHeight;
 
