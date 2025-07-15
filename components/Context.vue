18030cd03bfe3f895210b415577b735f850b23c6
Charlie Butcosk <incharge@weichbild.com>
2025-07-15 17:35:16 -0400

    Remove debug commenting

--- components/Context.vue
+++ components/Context.vue
@@ -3,7 +3,7 @@
     class="context"
     :class="[size ? `context--${ size }` : '', bordered == true ? 'context--bordered' : '']"
   >
-    <!-- <component :is="headingElement"
+    <component :is="headingElement"
       v-if="heading && button"
       class="context__heading"
       ref="heading"
@@ -21,8 +21,8 @@
           v-html="preppedHeading"
         />
       </NuxtLink>
-    </component> -->
-<!--     <component :is="headingElement"
+    </component>
+    <component :is="headingElement"
       v-else-if="heading"
       class="context__heading"
       ref="heading"
@@ -35,18 +35,17 @@
           aria-hidden="true"
           v-html="preppedHeading"
         />
-    </component> -->
-<!--       <div
-        v-if="subheading"
-        class="context__subheading"
-        v-html="preppedSubheading"
-        ref="subheading"
-      />
- -->      
+    </component>
+    <div
+      v-if="subheading"
+      class="context__subheading"
+      v-html="preppedSubheading"
+      ref="subheading"
+    />    
     <div
       v-if="subheading2"
       class="context__subheading"
-      v-html="subheading2"
+      v-html="preppedSubheading2"
       ref="subheading2"
     />
     <div
