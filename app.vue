33a65f3ec01bedc597221575267468e826c4ea28
Charlie Butcosk <incharge@weichbild.com>
2025-07-15 16:30:11 -0400

    Remove stray and unused axios import

--- app.vue
+++ app.vue
@@ -27,7 +27,6 @@
 </template>
 
 <script>
-import axios from 'axios';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
