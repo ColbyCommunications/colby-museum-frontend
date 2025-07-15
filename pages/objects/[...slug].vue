23c99773d68827419671949e2a259d95c828236f
Charlie Butcosk <incharge@weichbild.com>
2025-07-15 15:13:11 -0400

    Debug hydration: error-boundary objects page

--- pages/objects/[...slug].vue
+++ pages/objects/[...slug].vue
@@ -1,4 +1,13 @@
 <template>
+  <NuxtErrorBoundary @error="someErrorLogger">
+    <!-- You use the default slot to render your content -->
+    <template #error="{ error, clearError }">
+      You can display the error locally here: {{ error }}
+      <button @click="clearError">
+        This will clear the error.
+      </button>
+    </template>
+  </NuxtErrorBoundary>
   <div class="page page--object">
     <IntroContext
       :heading="artist"
