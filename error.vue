5a8d34c41791784bc574903512d858cda3e13a27
Charlie Butcosk <incharge@weichbild.com>
2025-07-18 08:31:12 -0400

    Stage error page to make potential error traces visible

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Oops! Something went wrong with that page. The error code was {{ error?.statusCode }}</h2>
+    <span>{{ error.message }}</span>
+    <div v-html="error.stack" />
+    <button @click="handleError">Go Home</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
