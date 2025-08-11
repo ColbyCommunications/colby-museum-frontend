87bb866b0726822f215d0d11bd78a3787d515dbd
Charlie Butcosk <incharge@weichbild.com>
2025-08-11 09:49:04 -0400

    Sanitize HTML injections in Context

--- components/global/PureHtml.vue
+++ components/global/PureHtml.vue
@@ -1,5 +1,6 @@
 <script>
   import { defineComponent,h } from 'vue';
+  import DOMPurify from 'isomorphic-dompurify'
 
   export default defineComponent({
     props: {
@@ -13,7 +14,7 @@
       },
     },
     setup(props, { slots }) {
-      return () => h(props.tag, { innerHTML: props.html });
+      return () => h(props.tag, { innerHTML: DOMPurify.sanitize(props.html) });
     },
   });
 </script>
