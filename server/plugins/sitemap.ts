62e1d69c2f090e874a3efc2562d47b8ab91f1525
Charlie Butcosk <incharge@weichbild.com>
2025-09-04 08:58:25 -0400

    Replace sitemap URL with deploy URL if in pages env and off-master

--- server/plugins/sitemap.ts
+++ server/plugins/sitemap.ts
@@ -1,10 +1,15 @@
 import { defineNitroPlugin } from 'nitropack/runtime';
 
+let deployUrl = 'https://museum.colby.edu/'
+if (process.env.CF_PAGES && process.env.CF_PAGES_BRANCH !== 'master') {
+    deployUrl = process.env.CF_PAGES_URL    
+}
+
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('sitemap:output', async (ctx) => {
         ctx.sitemap = ctx.sitemap.replaceAll(
             'https://museum-backend.colby.edu/',
-            'https://museum.colby.edu/'
-        ).replaceAll('https://colby-museum-frontend.pages.dev/','https://museum.colby.edu/');
+            deployUrl
+        ).replaceAll('https://colby-museum-frontend.pages.dev/', deployUrl);
     });
 });
